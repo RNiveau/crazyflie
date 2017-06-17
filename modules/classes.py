9366f62d0c615bdfb507afcaa8b5e05c41b5bebc
Romain Niveau <romain.niveau@gmail.com>
2017-06-17 19:59:15 +0200

    Next test

--- modules/classes.py
+++ modules/classes.py
@@ -2,31 +2,61 @@
 
 
 class Vector:
-    def __init__(self):
-        self.x = 0
-        self.y = 0
-        self.z = 0
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
 
     def __str__(self):
         return "{x=" + str(self.x) + ", y=" + str(self.y) + ", z=" + str(self.z) + "}"
 
 
 class Point:
-    def __init__(self):
-        self.roll = 0
-        self.pitch = 0
-        self.yaw = 0
+    def __init__(self, roll=0, pitch=0, yaw=0):
+        self.roll = roll
+        self.pitch = pitch
+        self.yaw = yaw
 
     def __str__(self):
-        return "{roll=" + str(self.roll) + ", pitch=" + str(self.pitch) + ", yaw=" + str(self.yaw) + "}"
+        return "{roll=" + str(self.roll) + ", pitch=" + str(self.pitch) + ", yaw=" + str(
+            self.yaw) + "}"
 
 
-class Position:
-    def __init__(self):
-        self.initPoint = None
-        self.point = Point()
-        self.accelerator = Vector()
-        self.gyro = Vector()
+class FlyData:
+    def __init__(self, point=None, accelerator=None, gyro=None):
+        self._point = point
+        self._accelerator = accelerator
+        self._gyro = gyro
 
     def __str__(self):
-        return "{initPoint=" + str(self.initPoint) + ", point=" + str(self.point) + ", accelerator=" + str(self.accelerator) + ", gyro=" + str(self.gyro) + "}"
+        return "{point=" + str(self._point) + ", accelerator=" + str(
+            self._accelerator) + ", gyro=" + str(self._gyro) + "}"
+
+    def accelerator(self, x, y, z):
+        self._accelerator = Vector(x, y, z)
+        return self
+
+    def gyro(self, x, y, z):
+        self._gyro = Vector(x, y, z)
+        return self
+
+    def point(self, roll=0, pitch=0, yaw=0, point=None):
+        if point is not None:
+            self._point = point
+        else:
+            self._point = Point(roll, pitch, yaw)
+        return self
+
+
+class CrazyflieContext:
+    def __init__(self):
+        self.init_point = None
+        self.last_data = None
+        self.fly_data = []
+
+    def __str__(self):
+        return "{init_point=" + str(self.init_point) + ", last_data=" + str(
+            self.last_data) + ", fly_data=" + str(''.join(str(e) for e in self.fly_data)) + "}"
+
+    def add_data(self, fly_data):
+        self.fly_data.append(fly_data)
--- stabilize.py
+++ stabilize.py
@@ -1,12 +1,14 @@
 import logging
 import time
+from functools import reduce
 from threading import Timer
 
 import cflib.crtp  # noqa
-from modules.classes import Point
-from modules.classes import Position
 from cflib.crazyflie import Crazyflie
 from cflib.crazyflie.log import LogConfig
+
+from modules.classes import CrazyflieContext, FlyData
+from modules.classes import Point
 
 logging.basicConfig(format="%(asctime)s %(levelname)s:%(name)s:%(message)s", level=logging.INFO)
 logger = logging.getLogger("StabilizeRun")
@@ -38,32 +40,37 @@ class StabilizeRun:
         # Variable used to keep main loop occupied until disconnect
         self.is_connected = True
 
+    def _startup(self):
+        # Unlock startup thrust protection
+        self._cf.commander.send_setpoint(0, 0, 0, 0)
+
     def _connected(self, link_uri):
         """ This callback is called form the Crazyflie API when a Crazyflie
         has been connected and the TOCs have been downloaded."""
         logger.info('Connected to %s' % link_uri)
-
-        self._stabilize = Position()
+        self._startup()
+        self._cf.commander.send_setpoint(0, 0, 0, 35000)
+        self._context = CrazyflieContext()
 
         # The definition of the logconfig can be made before connecting
         self._lg_stab = LogConfig(name='Stabilizer / Acc', period_in_ms=10)
         self._lg_stab.add_variable('stabilizer.roll', 'float')
         self._lg_stab.add_variable('stabilizer.pitch', 'float')
         self._lg_stab.add_variable('stabilizer.yaw', 'float')
-        self._lg_stab.add_variable('acc.x', 'float')
-        self._lg_stab.add_variable('acc.y', 'float')
-        self._lg_stab.add_variable('acc.z', 'float')
 
         self._lg_gyro = LogConfig(name='Gyro', period_in_ms=10)
+        self._lg_gyro.add_variable('acc.x', 'float')
+        self._lg_gyro.add_variable('acc.y', 'float')
+        self._lg_gyro.add_variable('acc.z', 'float')
         self._lg_gyro.add_variable('gyro.x', 'float')
         self._lg_gyro.add_variable('gyro.y', 'float')
         self._lg_gyro.add_variable('gyro.z', 'float')
 
-        self._lg_motor = LogConfig(name='Motors', period_in_ms=100)
-        self._lg_motor.add_variable('motor.m1', 'int32_t')
-        self._lg_motor.add_variable('motor.m2', 'int32_t')
-        self._lg_motor.add_variable('motor.m3', 'int32_t')
-        self._lg_motor.add_variable('motor.m4', 'int32_t')
+        # self._lg_motor = LogConfig(name='Motors', period_in_ms=100)
+        # self._lg_motor.add_variable('motor.m1', 'int32_t')
+        # self._lg_motor.add_variable('motor.m2', 'int32_t')
+        # self._lg_motor.add_variable('motor.m3', 'int32_t')
+        # self._lg_motor.add_variable('motor.m4', 'int32_t')
 
         # Adding the configuration cannot be done until a Crazyflie is
         # connected, since we need to check that the variables we
@@ -75,52 +82,80 @@ class StabilizeRun:
             # This callback will receive the data
             # self._lg_stab.data_received_cb.add_callback(self._stab_log_data)
             self._lg_stab.data_received_cb.add_callback(self._store_data)
-            #self._lg_stab.data_received_cb.add_callback(lambda x, y, z: (logger.info(self._stabilize.point)))
+            self._lg_stab.data_received_cb.add_callback(self._last_ten_point)
+            # self._lg_stab.data_received_cb.add_callback(lambda x, y, z: (logger.info(self._stabilize.point)))
 
             # self._lg_stab.data_received_cb.add_callback(self._send_position)
             #  self._lg_stab.data_received_cb.add_callback(self._stab)
             # self._lg_stab.data_received_cb.add_callback(self._send_position)
             # self._lg_gyro.data_received_cb.add_callback(self._stab_log_data)
             self._lg_gyro.data_received_cb.add_callback(self._store_data)
-#            self._lg_gyro.data_received_cb.add_callback(self._stab_log_data)
+            #            self._lg_gyro.data_received_cb.add_callback(self._stab_log_data)
             # self._lg_gyro.data_received_cb.add_callback(self._send_position)
             # self._lg_motor.data_received_cb.add_callback(self._stab_log_data)
             # This callback will be called on errors
             self._lg_stab.error_cb.add_callback(self._stab_log_error)
             # Start the logging
             self._lg_stab.start()
-          #  self._lg_gyro.start()
+            # self._lg_gyro.start()
             #   self._lg_motor.start()
         except KeyError as e:
             logger.error('Could not start log configuration,'
                          '{} not found in TOC'.format(str(e)))
         except AttributeError:
             logger.error('Could not add Stabilizer log config, bad configuration.')
-        self._cf.commander.send_setpoint(0, 0, 0, 30000)
+        Timer(1.9, self._startup).start()
         Timer(2, self._cf.close_link).start()
 
+    def _last_ten_point(self, timestamp, data, logconf):
+        if len(self._context.fly_data) >= 10:
+            subset = self._context.fly_data[
+                     len(self._context.fly_data) - 11: len(self._context.fly_data) - 1]
+            sum_point = reduce(add_point, subset, FlyData(point=Point()))
+            sum_point._point.roll /= 10
+            sum_point._point.pitch /= 10
+            sum_point._point.yaw /= 10
+            sum_point._point.roll -= self._context.init_point.roll
+            sum_point._point.pitch -= self._context.init_point.pitch
+            sum_point._point.yaw -= self._context.init_point.yaw
+            # self._cf.commander.send_setpoint(-sum_point._point.roll, -sum_point._point.pitch,
+            #                                  -sum_point._point.yaw, 33500)
+            print(sum_point)
+            print(self._context.init_point)
+            print(*subset)
+
     def _store_data(self, timestamp, data, logconf):
+        fly_data = None
+        if len(self._context.fly_data) > 0:
+            fly_data = self._context.fly_data[len(self._context.fly_data) - 1]
         if 'stabilizer.roll' in data:
-            self._stabilize.point.roll = data['stabilizer.roll']
-            self._stabilize.point.pitch = data['stabilizer.pitch']
-            self._stabilize.point.yaw = data['stabilizer.yaw']
-        if 'acc.x' in data:
-            self._stabilize.accelerator.x = data['acc.x']
-            self._stabilize.accelerator.y = data['acc.y']
-            self._stabilize.accelerator.z = data['acc.z']
-        if 'gyro.x' in data:
-            self._stabilize.gyro.x = data['gyro.x']
-            self._stabilize.gyro.y = data['gyro.y']
-            self._stabilize.gyro.z = data['gyro.z']
-        if self._stabilize.initPoint is None:
-            self._stabilize.initPoint = Point()
-            self._stabilize.initPoint.roll = data['stabilizer.roll']
-            self._stabilize.initPoint.pitch = data['stabilizer.pitch']
-            self._stabilize.initPoint.yaw = data['stabilizer.yaw']
+            point = Point(data['stabilizer.roll'], data['stabilizer.pitch'], data['stabilizer.yaw'])
+            self._context.last_data = point
+            if fly_data is None:
+                fly_data = FlyData()
+                fly_data.point(point)
+                self._context.add_data(fly_data)
+            elif fly_data.point is not None:
+                fly_data = FlyData()
+                fly_data.point(point=point)
+                self._context.add_data(fly_data)
+            else:
+                fly_data.point(point=point)
+        # if 'acc.x' in data:
+        #     self._context.accelerator.x = data['acc.x']
+        #     self._context.accelerator.y = data['acc.y']
+        #     self._context.accelerator.z = data['acc.z']
+        # if 'gyro.x' in data:
+        #     self._context.gyro.x = data['gyro.x']
+        #     self._context.gyro.y = data['gyro.y']
+        #     self._context.gyro.z = data['gyro.z']
+        if self._context.init_point is None and 'stabilizer.roll' in data:
+            self._context.init_point = Point(data['stabilizer.roll'], data['stabilizer.pitch'],
+                                             data['stabilizer.yaw'])
 
     def _send_position(self, timestamp, data, logconf):
-        roll = self._stabilize.initPoint.roll - self._stabilize.point.roll
-        pitch = self._stabilize.initPoint.pitch - self._stabilize.point.pitch
+        roll = self._context.init_point.roll - self._context.lastPoint.roll
+        pitch = self._context.init_point.pitch - self._context.lastPoint.pitch
         logger.info("Send position %f %f %f" % (roll, pitch, 0))
         self._cf.commander.send_setpoint(roll, pitch, 0, 33500)
 
@@ -151,6 +186,12 @@ class StabilizeRun:
         """Callback when the Crazyflie is disconnected (called in all cases)"""
         logger.info('Disconnected from %s' % link_uri)
         self.is_connected = False
+
+
+def add_point(fly_data, fly_data2):
+    return FlyData(point=Point(fly_data._point.roll + fly_data2._point.roll,
+                               fly_data._point.pitch + fly_data2._point.pitch,
+                               fly_data._point.yaw + fly_data2._point.yaw))
 
 
 if __name__ == '__main__':
